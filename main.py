977d29d9d3b037e18b65b210c586bda2016474cb
VagggelisAn <vaggelis.a.g.m@gmail.com>
2025-09-05 11:13:13 +0300

    Added usage example and instructions

--- main.py
+++ main.py
@@ -1,81 +1,24 @@
 import os
 import importlib.util
-import tensorflow as tf
+# import tensorflow as tf
 import numpy as np
 import hls4ml
-from configs import ExperimentConfig, HLS4MLConfig
-from dataclasses import asdict, replace
 import pprint
+from dataclasses import replace
+
+# - - - Custom Modules - - -
+from configs import default_experiment, parametrize_hls4ml_config, parametrize_hls4ml_converter, global_config, \
+                    remove_experiments, run_experiments
+from sweep import sweep
 
 # - - - Global Variables - - -
-# Default experiment values:
-default_experiment = ExperimentConfig(
-    hls4ml_config=HLS4MLConfig(
-        granularity='model',
-        backend=None,
-        default_precision='fixed<16,6>',
-        default_reuse_factor=1,
-        max_precision=None
-    )
-)
-# Experiments
-# One entry for every value entered by user. 
-# eg. if user enters: hls4ml_config granularity model name and then hls4ml_config default_reuse_factor 1 2 4 then 2+3=5 copies of experiments is created.
-experiments = []
+# experiments = []
 
-
-def parametrize_hls4ml_config():
-    global experiments, default_experiment        
-
-    #   - - - User Parametrization - - -
-    while True:
-        current_experiment = default_experiment
-        print("Default hls4ml config values:")
-        pprint.pprint(default_experiment)
-
-    #   Receive user input and remove the ">>> " string from the start, split user input into strings.
-    #   First string should be the argument selected and the rest of the strings should be the set of the values that the argument will take.
-        user_input = input(">>> ").strip()
-        user_input = user_input.split()
-        argument = user_input[0]
-        values = user_input[1:]
-    #   Eliminate duplicates
-        values = list(dict.fromkeys(values))
-
-    #   Make sure user_input, argument and values are not empty (values can be empty for the back/ return option)
-        if not user_input or not argument or (not values and argument not in ["back", "return", "b", "r"]):
-            print(f"Syntax: [hls4ml_config argument]: value1 value2 value3...\neg. default_reuse_factor: 1, 2, 4, 8")
-            continue
-
-    #   Make sure that the arguments will receive proper values
-        elif argument == "granularity":
-            for value in values:
-                # Since dataclasses are frozen, we have to use the replace function to change a value:
-                replace(current_experiment, hls4ml_config=replace(current_experiment.hls4ml_config, granularity=value))
-                experiments.append(current_experiment)
-        elif argument == "default_precision":
-            for value in values:
-                replace(current_experiment, hls4ml_config=replace(current_experiment.hls4ml_config, default_precision=value))
-                experiments.append(current_experiment)
-                
-        # elif args[0] == "backend":
-        # elif args[0] == "default_precision":
-        # elif args[0] == "default_reuse_factor":
-        # elif args[0] == "max_precision":
-
-        elif argument in ["back", "return", "b", "r"]:
-            break
-        else:
-            print(f"Syntax: [hls4ml_config argument]: value1, value2, value3...\neg. default_reuse_factor: 1, 2, 4, 8")
-            continue
-        print(f"{argument} updated.")
-
-
-    return hls4ml
-    
-
-##      Main        ##
+## - - - Main - - - ##
 def main():
+    marked_parameters = {}
+    global_parameters = replace(default_experiment.global_parameters)
+    experiments = []
 
 #   - - - User Parametrization - - -
 #   Note that ml2hls does not check for correct values for the arguments, the respective libraries are responsible for that.
@@ -84,22 +27,37 @@ def main():
     while True:
         # Append an experiment with default settings to the list of experiments
         print("Select option:\n\
-                hls4ml_config: create a set of values to be sweeped from the hls4ml config arguments.")
+            hls4ml_config: create a set of values to be sweeped from the hls4ml config arguments.\n\
+            hls4ml_converter: create a set of values to be sweeped from the hls4ml converter arguments.\n\
+            global_config: create a set of global configuration arguments.\n\
+            sweep: generate all combinations of the marked parameters.\n\
+            print_marked: print the currently marked parameters.\n\
+            print_experiments: print the currently generated experiments.\n\
+            run: execute the generated experiments.\n\
+            quit: exit the program.")
         user_input = input(">>> ").strip()
         if not user_input:
             continue
         elif user_input in ["hls4ml_config", "hc"]:
-            parametrize_hls4ml_config()
+            parametrize_hls4ml_config(marked_parameters)
+        elif user_input in ["hls4ml_converter", "hcv"]:
+            parametrize_hls4ml_converter(marked_parameters)
+        elif user_input in ["global_config", "gc"]:
+            global_parameters = global_config()
+        elif user_input in ["sweep", "s"]:
+            experiments = sweep(marked_parameters, global_parameters)
+        elif user_input in ["print_marked", "pm"]:
+            pprint.pprint(marked_parameters, sort_dicts=False)
+        elif user_input in ["print_experiments", "pe"]:
+            pprint.pprint(experiments, sort_dicts=False)
+        elif user_input in ["run", "execute", "e"]:
+            run_experiments(experiments)
         elif user_input in ["quit", "exit", "q", "e"]:
             break
-        elif user_input in ["print", "p"]:
-            pprint.pprint(experiments)
         else:
             print(f"Option {user_input} does not exist.")
             continue
-#       - - - Sweep range - - -
-
-    # config = hls4ml.utils.config_from_keras_model(model, **hls4ml_config)
 
 if __name__ == "__main__":
     main()
+
